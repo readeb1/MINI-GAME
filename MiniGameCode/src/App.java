a82f6ef5876d0b8d71fb0ca6b1434d52ceedd6e1
readeb1 <128709099+readeb1@users.noreply.github.com>
2023-05-29 09:45:26 -0400

    Updated Today

--- MiniGameCode/src/App.java
+++ MiniGameCode/src/App.java
@@ -7,22 +7,23 @@ public class App {
 
         Scanner in = new Scanner(System.in);
 
-        int sudoku[][] = {
-            {11, 11, 1, 2, 3, 4, 11},
-            {11, 10, 9, 9, 9, 9, 10},
-            {5, 10, 4, 3, 1, 2, 10},
-            {6, 10, 1, 2, 3, 4, 10},
-            {7, 10, 3, 4, 2, 1, 10},
-            {8, 10, 2, 1, 4, 0, 10},
-            {11, 10, 9, 9, 9, 9, 10}};
+        int sudoku[][] = 
         // {
-        //         { 11, 11, 1, 2, 3, 4, 11 },
-        //         { 11, 10, 9, 9, 9, 9, 10 },
-        //         { 5, 10, 4, 3, 0, 0, 10 },
-        //         { 6, 10, 1, 2, 3, 0, 10 },
-        //         { 7, 10, 0, 0, 2, 0, 10 },
-        //         { 8, 10, 2, 1, 0, 0, 10 },
-        //         { 11, 10, 9, 9, 9, 9, 10 } };
+        //     {11, 11, 1, 2, 3, 4, 11},
+        //     {11, 10, 9, 9, 9, 9, 10},
+        //     {5, 10, 4, 3, 1, 2, 10},
+        //     {6, 10, 1, 2, 3, 4, 10},
+        //     {7, 10, 3, 4, 2, 1, 10},
+        //     {8, 10, 2, 1, 4, 0, 10},
+        //     {11, 10, 9, 9, 9, 9, 10}};
+        {
+                { 11, 11, 1, 2, 3, 4, 11 },
+                { 11, 10, 9, 9, 9, 9, 10 },
+                { 5, 10, 4, 3, 0, 0, 10 },
+                { 6, 10, 1, 2, 3, 0, 10 },
+                { 7, 10, 0, 0, 2, 0, 10 },
+                { 8, 10, 2, 1, 0, 0, 10 },
+                { 11, 10, 9, 9, 9, 9, 10 } };
 
         // ArraysUtils.printArray(sudoku);
         printArray(sudoku);
@@ -30,9 +31,11 @@ public class App {
         boolean failed = false;
 
         while (!isSudokuComplete(sudoku)) {
-            System.out.println("What number would you like to place? Enter the box (ex. A 4 3)");
+            System.out.println(blue("What number would you like to place? Enter the box (ex. A 4 3)"));
+            
             String change = in.nextLine();
             String[] stringChange = change.split(" ");
+            stringChange[0] = stringChange[0].toUpperCase();
 
             int row = stringChange[0].charAt(0) - 'A' + 2;
             int col = Integer.parseInt(stringChange[1]) + 1;
@@ -42,10 +45,10 @@ public class App {
                 continue;
             }
 
-            if (sudoku[row][col] != 0) {
-                System.out.println("The selected box is already filled! Please choose another box.");
-                continue;
-            }
+            // if (sudoku[row][col] != 0) {
+            //     System.out.println("The selected box is already filled! Please choose another box.");
+            //     continue;
+            // }
 
             int number = Integer.parseInt(stringChange[2]);
             sudoku[row][col] = number;
@@ -58,9 +61,9 @@ public class App {
             System.out.println("You failed! The Sudoku puzzle is not completed correctly.");
         } else {
             System.out.println("Congratulations! You have completed the Sudoku puzzle!");
-            String[] notes = {"C4", "C4", "G4", "G4", "A4", "A4", "G4"};
+            String[] notes = {"C4", "C4", "G4", "G4", "A4", "A4", "G4","C4","C4", "G4", "G4", "A4", "A4", "G4","C4","C4", "G4", "G4", "A4", "A4", "G4","C4","C4", "G4", "G4", "A4", "A4", "G4"};
         for (int i = 0; i < notes.length; i++){
-        MusicNote.playNote(notes[i], 0.25);
+        MusicNote.playNote(notes[i], .25);
         }
 
     }
@@ -72,9 +75,9 @@ public class App {
                 if (array[i][j] >= 0 && array[i][j] <= 4) {
                     System.out.print(" " + array[i][j] + " ");
                 } else if (array[i][j] == 9) {
-                    System.out.print("\033[0;34m" + "---" + "\033[0m");
+                    System.out.print(blue("---"));
                 } else if (array[i][j] == 10) {
-                    System.out.print("\033[0;34m" + " | " +"\033[0m");
+                    System.out.print(blue(" | "));
                 } else if (array[i][j] == 5) {
                     System.out.print(" A ");
                 } else if (array[i][j] == 6) {
