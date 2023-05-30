4feb57d0d73e4440c1790b0ea5d2d678c49d5a3e
readeb1 <128709099+readeb1@users.noreply.github.com>
2023-05-30 08:53:03 -0400

    8:52 Tuesday

--- MiniGameCode/src/App.java
+++ MiniGameCode/src/App.java
@@ -3,42 +3,50 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to Suduko!\n");
+        System.out.println("Welcome to Suduko!\nTo win the game you must fill all the values correctly!\nYou are to replace the 0's with 1,2,3,4 every horizontal and vertical line should include these 4 values\nGoodluck!!!\n ");
 
         Scanner in = new Scanner(System.in);
 
-        int sudoku[][] = 
-        // {
-        //     {11, 11, 1, 2, 3, 4, 11},
-        //     {11, 10, 9, 9, 9, 9, 10},
-        //     {5, 10, 4, 3, 1, 2, 10},
-        //     {6, 10, 1, 2, 3, 4, 10},
-        //     {7, 10, 3, 4, 2, 1, 10},
-        //     {8, 10, 2, 1, 4, 0, 10},
-        //     {11, 10, 9, 9, 9, 9, 10}};
-        {
-                { 11, 11, 1, 2, 3, 4, 11 },
-                { 11, 10, 9, 9, 9, 9, 10 },
-                { 5, 10, 4, 3, 0, 0, 10 },
-                { 6, 10, 1, 2, 3, 0, 10 },
-                { 7, 10, 0, 0, 2, 0, 10 },
-                { 8, 10, 2, 1, 0, 0, 10 },
-                { 11, 10, 9, 9, 9, 9, 10 } };
+        int sudoku[][] =
+        // Main Gameboard
+                // {
+                // {11, 11, 1, 2, 3, 4, 11},
+                // {11, 10, 9, 9, 9, 9, 10},
+                // {5, 10, 4, 3, 1, 2, 10},
+                // {6, 10, 1, 2, 3, 4, 10},
+                // {7, 10, 3, 4, 2, 1, 10},
+                // {8, 10, 2, 1, 4, 0, 10},
+                // {11, 10, 9, 9, 9, 9, 10}};
+                // Array for easily testing the game
 
-        // ArraysUtils.printArray(sudoku);
+                {
+                        { 11, 11, 1, 2, 3, 4, 11 },
+                        { 11, 10, 9, 9, 9, 9, 10 },
+                        { 5, 10, 4, 3, 0, 0, 10 },
+                        { 6, 10, 1, 2, 3, 0, 10 },
+                        { 7, 10, 0, 0, 2, 0, 10 },
+                        { 8, 10, 2, 1, 0, 0, 10 },
+                        { 11, 10, 9, 9, 9, 9, 10 } };
+        // Game board Array
+
         printArray(sudoku);
 
-        boolean failed = false;
+        
 
         while (!isSudokuComplete(sudoku)) {
-            System.out.println(blue("What number would you like to place? Enter the box (ex. A 4 3)"));
-            
+            // While loop to allow user to continue playing until puzzle is solved
+            System.out.println("What number would you like to place? Enter the box (ex. A 4 3)");
+
             String change = in.nextLine();
+            // User input
             String[] stringChange = change.split(" ");
+            // Split into a string array so it's easier to use in methods.
             stringChange[0] = stringChange[0].toUpperCase();
+            //Allow user to input lower and upper
 
             int row = stringChange[0].charAt(0) - 'A' + 2;
             int col = Integer.parseInt(stringChange[1]) + 1;
+            // To format the game board to replace the correct values
 
             if (row < 0 || row >= sudoku.length || col < 0 || col >= sudoku[0].length) {
                 System.out.println("Invalid input! Please enter a valid box.");
@@ -46,30 +54,38 @@ public class App {
             }
 
             // if (sudoku[row][col] != 0) {
-            //     System.out.println("The selected box is already filled! Please choose another box.");
-            //     continue;
+            // System.out.println("The selected box is already filled! Please choose another
+            // box.");
+            // continue;
             // }
+            // Code for if I wanted them to only be allowed to change a number once.
 
             int number = Integer.parseInt(stringChange[2]);
             sudoku[row][col] = number;
+            // New value their assigning
 
             printArray(sudoku);
 
         }
 
-        if (failed) {
-            System.out.println("You failed! The Sudoku puzzle is not completed correctly.");
-        } else {
-            System.out.println("Congratulations! You have completed the Sudoku puzzle!");
-            String[] notes = {"C4", "C4", "G4", "G4", "A4", "A4", "G4","C4","C4", "G4", "G4", "A4", "A4", "G4","C4","C4", "G4", "G4", "A4", "A4", "G4","C4","C4", "G4", "G4", "A4", "A4", "G4"};
-        for (int i = 0; i < notes.length; i++){
-        MusicNote.playNote(notes[i], .25);
+        System.out.println("Congratulations! You have completed the Sudoku puzzle!");
+        // Winnning condition plays a song using an array
+        String[] notes = { "C4", "C4", "G4", "G4", "A4", "A4", "G4", "C4", "C4", "G4", "G4", "A4", "A4", "G4", "C4",
+                "C4", "G4", "G4", "A4", "A4", "G4", "C4", "C4", "G4", "G4", "A4", "A4", "G4" };
+        for (int i = 0; i < notes.length; i++) {
+            MusicNote.playNote(notes[i], .25);
+
         }
 
     }
-    }
 
-    public static void printArray(int [][] array) {
+    /**
+     * The method creates the sudoku game using different values to create an easily
+     * understandble map.
+     * 
+     * @param array Is the array values of sudoku that the game uses
+     */
+    public static void printArray(int[][] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[0].length; j++) {
                 if (array[i][j] >= 0 && array[i][j] <= 4) {
@@ -89,6 +105,7 @@ public class App {
                 } else if (array[i][j] == 11) {
                     System.out.print("   ");
                 }
+                // Using an array for the map design made it easier to plan out the game
 
             }
             System.out.println();
@@ -96,6 +113,13 @@ public class App {
         System.out.println();
     }
 
+    /**
+     * This method is used to check if the user has won the game
+     * 
+     * @param sudoku Is the game board/array that's being used
+     * @return Return win condition or continue playing the game if still not
+     *         satisfied
+     */
     public static boolean isSudokuComplete(int[][] sudoku) {
         int sudokuCorrect[][] = {
                 { 11, 11, 1, 2, 3, 4, 11 },
@@ -105,19 +129,26 @@ public class App {
                 { 7, 10, 3, 4, 2, 1, 10 },
                 { 8, 10, 2, 1, 4, 3, 10 },
                 { 11, 10, 9, 9, 9, 9, 10 } };
+        // Sudoku correct or filled bored which is checked with the users inputs
 
         for (int i = 0; i < sudoku.length; i++) {
             for (int j = 0; j < sudoku[0].length; j++) {
                 if (sudoku[i][j] != sudokuCorrect[i][j]) {
                     return false;
+                    // Checks the correct one
                 }
             }
         }
         return true;
     }
-    
-    public static String blue(String text){
-        return "\033[0;34m" + text +"\033[0m";
+
+    /**
+     * Turns the text I chose blue
+     * @param text Takes the text or string used
+     * @return Makes it blue 
+     */
+    public static String blue(String text) {
+        return "\033[0;34m" + text + "\033[0m";
     }
 
 }
---
+++ MiniGameCode/src/TryingTwoLevels.java
@@ -0,0 +1,148 @@
+import java.util.Scanner;
+
+public class TryingTwoLevels {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to Sudoku!\n");
+
+        Scanner in = new Scanner(System.in);
+
+        int level1[][] = {
+            { 11, 11, 1, 2, 3, 4, 11 },
+            { 11, 10, 9, 9, 9, 9, 10 },
+            { 5, 10, 4, 3, 0, 0, 10 },
+            { 6, 10, 1, 2, 3, 0, 10 },
+            { 7, 10, 0, 0, 2, 0, 10 },
+            { 8, 10, 2, 1, 0, 0, 10 },
+            { 11, 10, 9, 9, 9, 9, 10 }
+        };
+
+        int level2[][] = {
+            { 11, 11, 1, 2, 3, 4, 11 },
+            { 11, 10, 9, 9, 9, 9, 10 },
+            { 5, 10, 0, 0, 3, 1, 10 },
+            { 6, 10, 0, 3, 0, 0, 10 },
+            { 7, 10, 3, 0, 0, 2, 10 },
+            { 8, 10, 2, 0, 4, 0, 10 },
+            { 11, 10, 9, 9, 9, 9, 10 }
+        };
+
+        while (true) {
+            System.out.println("Please select a level:");
+            System.out.println("1. Level 1");
+            System.out.println("2. Level 2");
+            System.out.println("0. Quit");
+
+            int choice = in.nextInt();
+            in.nextLine();
+
+            if (choice == 0) {
+                System.out.println("Thank you for playing! Goodbye!");
+                break;
+            } else if (choice == 1) {
+                playLevel(level1, choice);
+            } else if (choice == 2) {
+                playLevel(level2, choice);
+            } else {
+                System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+
+    public static void playLevel(int[][] sudoku, int level) {
+        Scanner in = new Scanner(System.in);
+        boolean completed = false;
+
+        while (!completed) {
+            printArray(sudoku);
+
+            System.out.println("What number would you like to place? Enter the box (ex. A 4 3)");
+
+            String change = in.nextLine();
+            String[] stringChange = change.split(" ");
+            stringChange[0] = stringChange[0].toUpperCase();
+
+            int row = stringChange[0].charAt(0) - 'A' + 2;
+            int col = Integer.parseInt(stringChange[1]) + 1;
+
+            if (row < 0 || row >= sudoku.length || col < 0 || col >= sudoku[0].length) {
+                System.out.println("Invalid input! Please enter a valid box.");
+                continue;
+            }
+
+            int number = Integer.parseInt(stringChange[2]);
+            sudoku[row][col] = number;
+
+            completed = isSudokuComplete(sudoku, level);
+        }
+
+        System.out.println("Congratulations! You have completed the Sudoku puzzle!");
+        
+    }
+
+    public static void printArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                if (array[i][j] >= 0 && array[i][j] <= 4) {
+                    System.out.print(" " + array[i][j] + " ");
+                } else if (array[i][j] == 9) {
+                    System.out.print(blue("---"));
+                } else if (array[i][j] == 10) {
+                    System.out.print(blue(" | "));
+                } else if (array[i][j] == 5) {
+                    System.out.print(" A ");
+                } else if (array[i][j] == 6) {
+                    System.out.print(" B ");
+                } else if (array[i][j] == 7) {
+                    System.out.print(" C ");
+                } else if (array[i][j] == 8) {
+                    System.out.print(" D ");
+                } else if (array[i][j] == 11) {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static boolean isSudokuComplete(int[][] sudoku, int level) {
+        int[][] sudokuCorrect;
+
+        if (level == 1) {
+            sudokuCorrect = new int[][] {
+                { 11, 11, 1, 2, 3, 4, 11 },
+                { 11, 10, 9, 9, 9, 9, 10 },
+                { 5, 10, 4, 3, 1, 2, 10 },
+                { 6, 10, 1, 2, 3, 4, 10 },
+                { 7, 10, 3, 4, 2, 1, 10 },
+                { 8, 10, 2, 1, 4, 3, 10 },
+                { 11, 10, 9, 9, 9, 9, 10 }
+            };
+        } else if (level == 2) {
+            sudokuCorrect = new int[][] {
+                { 11, 11, 1, 2, 3, 4, 11 },
+                { 11, 10, 9, 9, 9, 9, 10 },
+                { 5, 10, 4, 2, 3, 1, 10 },
+                { 6, 10, 1, 3, 2, 4, 10 },
+                { 7, 10, 3, 4, 1, 2, 10 },
+                { 8, 10, 2, 1, 4, 3, 10 },
+                { 11, 10, 9, 9, 9, 9, 10 }
+            };
+        } else {
+            throw new IllegalArgumentException("Invalid Level!");
+        }
+
+        for (int i = 0; i < sudoku.length; i++) {
+            for (int j = 0; j < sudoku[0].length; j++) {
+                if (sudoku[i][j] != 0 && sudoku[i][j] != sudokuCorrect[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static String blue(String text){
+        return "\033[0;34m" + text +"\033[0m";
+    }
+}
